8fb9ab74283b2e3af04fb63aa95199011abd1725
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2019-01-24 00:11:52 -0500

    Improve params for database installation

--- sql/populate_database.sh
+++ sql/populate_database.sh
@@ -9,8 +9,13 @@ if [ -z "$SUDO" ]; then
   SUDO=''
 fi
 
+# rationale: set a default user to execute script
+if [ -z "$DATABASE_EXECUTE_USER" ]; then
+  DATABASE_EXECUTE_USER='postgres'
+fi
+
 # rationale: if table not exists, execute populations, if not have error, the query was successfull
-SQLResult=$(echo "SELECT * FROM ${DATABSE_TEST_TABLE} LIMIT 0;" | $SUDO psql  -p ${DATABASE_PORT} -U postgres -d ${DATABASE_NAME} 2>&1) # error and output to variable
+SQLResult=$(echo "SELECT * FROM ${DATABASE_TEST_INSTALLED_TABLE} LIMIT 0;" | $SUDO psql  -p ${DATABASE_PORT} -U postgres -d ${DATABASE_NAME} 2>&1) # error and output to variable
 if ! echo "$SQLResult" | grep 'ERROR' &>/dev/null
 then
   echo 'La tabla parametro ya está creada. Nada que hacer.'
@@ -23,6 +28,6 @@ cp "$SQL_INSIDE_DB" "$tmp_file_inside_db"
 $SUDO chown postgres:postgres "$tmp_file_inside_db"
 $SUDO su postgres -c "
 cd /tmp
-psql -p ${DATABASE_PORT} -f ${tmp_file_inside_db} -d ${DATABASE_NAME}
+psql -U ${DATABASE_EXECUTE_USER} -p ${DATABASE_PORT} -f ${tmp_file_inside_db} -d ${DATABASE_NAME}
 "
 fi
