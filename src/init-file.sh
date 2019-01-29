2e325f505c12085c75e9458be1fc3f718291ad05
Jorge Ulises <naturalmentejorge@gmail.com>
2019-01-29 10:41:20 -0500

    Update init-file.sh

--- src/init-file.sh
+++ src/init-file.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \$ "
+PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\e[31mdocker\e[0m:\[\033[01;34m\]\w\[\033[00m\] \$ "
 cd /htdocs
