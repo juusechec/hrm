db1ea594cefcd387aaacf1e66dc871ab89dc7d15
gitcoz <alejohermes@gmail.com>
2019-03-28 22:41:12 -0500

    mejora de la visualizacion formulario  conceppto  examen mexico

--- src/src/Form/ConceptoExamenMedicoType.php
+++ src/src/Form/ConceptoExamenMedicoType.php
@@ -6,15 +6,28 @@ use App\Entity\ConceptoExamenMedico;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ConceptoExamenMedicoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nombre')
-            ->add('descripcion')
-            ->add('abreviacion')
+            ->add('nombre',  TextType::class, [
+                'attr' => [ 'class' => 'form-control' ],
+                'label_attr' => [ 'class' => 'form-label' ],
+                'required' => false
+            ])
+            ->add('descripcion',  TextType::class, [
+                'attr' => [ 'class' => 'form-control' ],
+                'label_attr' => [ 'class' => 'form-label' ],
+                'required' => false
+            ])
+            ->add('abreviacion',  TextType::class, [
+                'attr' => [ 'class' => 'form-control' ],
+                'label_attr' => [ 'class' => 'form-label' ],
+                'required' => false
+            ])
             ->add('orden')
             ->add('activo')
         ;
